f2857cf235ad9c8918baf07df1a1a5d07f46e82f
lookforhan <31067305+lookforhan@users.noreply.github.com>
2018-11-07 16:51:20 +0800

    更新文件
    1生成最终可以运行的INP文件：anytown_05.inp
    2更新.gitignore文件

--- feature_create_new_inp/create_new_inp.m
+++ feature_create_new_inp/create_new_inp.m
@@ -47,6 +47,8 @@ errcode=calllib(libName,'ENsaveinpfile',out_inp_0);% 见行14
 if errcode~=0
     keyboard
 end
+errcode=calllib(libName,'ENclose');% 关闭文件
+unloadlibrary(libName) % 卸载动态链接库
 % 1 修改单位
 % 在库Unit_conversion中GPM2LPS.m函数可以转化数据单位 
 % 调用GPM2LPS.m函数，可以完成单位修改。
@@ -60,10 +62,17 @@ if errcode ~=0
 end
 
 % 2 删除tank
-
+% 删除tank较为简单，采用人工手动删除，删除后文件保存为：out_inp_2 
+% 2.1 在[TANKS]中删除41、42
+% 2.1 在[PIPES]中删除涉及21、42的管道
+% 2.3 在[COORDINATE]中删除41、42
 % 3 删除pump
-
+% 删除pump较为简单，采用人工手动删除，删除后文件保存为：out_inp_3 
+% 3.1 在[PUMPS]行中删除78、79、80
+% 3.2 在[ENERGY]中删除79、79、80
+% 3.3 在[PIPES]中增加一行连接源点40与节点20，
+% “126  40  20  1   762 130 0”起始节点为40，终止节点为20，管长为1m，管径为762mm,粗糙系数为130，局部损失系数为0
 % 4 无需修改
-% 5 结束
-errcode=calllib(libName,'ENclose');% 关闭文件
-unloadlibrary(libName) % 卸载动态链接库
+% 5 发现修改后出现一些问题，管网本身出现负压。即out_inp_3文件在标准EPANET 2应用中进行平差计算，应用给出警告信息，即出现负压。为了解决负压问题，对inp文件进行一些修改。
+% 5.1 管道110、113、114、115、116、125管径太小为：0.002540mm，修改为203.2mm
+% 5.2 源点水头为3.048m,太低，改为263.048m
