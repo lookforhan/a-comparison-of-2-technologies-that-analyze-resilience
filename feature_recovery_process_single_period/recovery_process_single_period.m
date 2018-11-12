b5c44cb3cee0edf2b63336b1f43a727c467e71c6
lookforhan <31067305+lookforhan@users.noreply.github.com>
2018-11-12 19:26:14 +0800

    第一次更新single-period

--- feature_recovery_process_single_period/recovery_process_single_period.m
+++ feature_recovery_process_single_period/recovery_process_single_period.m
@@ -1,4 +1,4 @@
-% name:recovery_process_eps.m
+% name:recovery_process_single_period.m
 % net: MOD_5.inp
 % pro-process
 % process
@@ -43,7 +43,7 @@ else
 end
 [ t3_2,damage_data ] = read_damage_info( damage_file );% from 'damageNet\'
 if t3_2
-    keyboard    
+    keyboard
 end
 fid3_3 = fopen(pdd_file,'r');
 pdd_data_1 = textscan(fid3_3,'%s%f%f','delimiter','|','headerlines',1);
@@ -55,7 +55,7 @@ pdd_data{1,3} = pdd_data_1{3};
 % process_4 generate damage_net
 [t4_2,damage_pipe_info] = ND_Execut_deterministic1(net_data,damage_data);% from 'damageNet\'
 if t4_2
-    keyboard    
+    keyboard
 end
 [t3,pipe_relative] = damageNetInp2_GIRAFFE2(net_data,damage_pipe_info,EPA_format,damage_net);% from 'damageNet\'
 if t3
@@ -69,13 +69,13 @@ Dp_Travel_mat=Dp_Travel_mat1*0;% 不考虑修复队伍移动时间的影响。
 BreakPipePriority = BreakPipe_order;% 修复次序
 RepairCrew = {'a'};
 output_result_dir = out_dir;
+
 [BreakPipe_result,RepairCrew_result]=priorityList2schedule6(BreakPipePriority,RepairCrew,BreakPipe_order,...
     Dp_Inspect_mat,Dp_Repair_mat,Dp_Travel_mat,output_result_dir);%根据修复次序生成时间表 from 'random\'
 [PipeStatus2,PipeStatusChange]=schedule2pipestatus3(BreakPipe_result);%根据时间表生成管道状态矩阵 from 'random\'
 PipeStatus = PipeStatus2;
 duration_one = numel(PipeStatus(1,:));
-
-% process_6 eps simulation
+% pprocess_6 generate original inp file
 errcode1 = calllib(lib_name,'ENopen',damage_net,[out_dir,'damage.rpt'],'');% from 'EPANETx64PDD.dll'
 if errcode1~=0
     keyboard
@@ -91,104 +91,36 @@ for j_i = 1:numel(node_id)
     errcode6_3 = calllib(lib_name,'ENsetnodevalue',index,121,Hcritical(j_i));
 end
 errcode6_4 = calllib(lib_name,'ENsaveinpfile',temp_inp_file);
-%
-durationSet = (duration_one)*3600;%(s)
-% durationSet = 24*7*3600;%(s)
-calllib(lib_name,'ENsettimeparam',0,durationSet);
-calllib(lib_name,'ENsetoption',0,500);
-errcode2 = calllib(lib_name,'ENopenH');
-errcode3 = calllib(lib_name,'ENinitH',0);
-temp_t = 0;
-temp_tstep = 1;
-time_step_n =0;
-% PipeStatus = [pipeStatus];
-% [newPipeStatus,temeStepChosen] = pipeStatusChange(PipeStatus);
-[newPipeStatus ,timeStepChose]= pipeStatusChange(PipeStatus);
-[newPipeStatusChange ,timeStepChangeChose]= pipeStatusChange(PipeStatusChange);
-errcode4 = 0;
-while (temp_tstep && ~errcode4)
-    time_step_n = time_step_n+1;
-%     [errcode4,temp_t] = calllib(lib_name,'ENrunH',temp_t);
-    
-    if errcode4
-        keyboard
+errcode6_5 = calllib(lib_name,'ENclose');
+% process_7 single-period simulation
+timeStep_end=numel(PipeStatus(1,:));%时间步数
+for timeStep_i=1:timeStep_end
+    PipeStatus_timeStep=PipeStatus(:,timeStep_i);
+    [outdata]=timeStepNet(PipeStatus_timeStep,damage_pipe_info,net_data);%输出当前时间步的管道状态管网信息。
+    output_net_filename_n =['.\results\',num2str(timeStep_i),'.inp'];
+    output_net_rpt = ['.\results\',num2str(timeStep_i),'.rpt'];
+    output_net_pdd_file_n = ['.\results\','pdd_',num2str(timeStep_i)];
+    errcode7_1=Write_Inpfile4(net_data,EPA_format,outdata,output_net_filename_n);% 写入新管网inp
+    inputdata=output_net_filename_n;
+    errcode7_2 = calllib(lib_name,'ENopen',damage_net,output_net_rpt ,'');% from 'EPANETx64PDD.dll'
+    for j_i = 1:numel(node_id)
+        [errcode7_3,cstring,index]=calllib(lib_name,'ENgetnodeindex',node_id{j_i},index);
+        errcode7_4 = calllib(lib_name,'ENsetnodevalue',index,120,Hminimum(j_i));
+        errcode7_5 = calllib(lib_name,'ENsetnodevalue',index,121,Hcritical(j_i));
     end
-    
-    
-%     [~,real_pre]=Get_EPANETx64PDD(lib_name,junction_num,11);%水压
-    
-    
-    
-    %
-    disp(num2str(temp_t))
-    disp(num2str(time_step_n))
-    [lia,loc] = ismember(time_step_n,timeStepChose);%
-    if lia
-        fprintf(fid,'%s\r\n','开始修改管道状态');
-        str1 = '';
-        str2 = '';
-        str3 = '';
-        mid_status = newPipeStatusChange(:,loc);
-        for i = 1:numel(mid_status)
-            pipe_status = mid_status(i);
-            switch  pipe_status
-                case 0
-                    continue %
-                case 1
-                    % isolation
-                    str2 = [str2,'隔离管道',pipe_relative{i,1},';'];
-                    for j =1:numel(pipe_relative{i,2})% 隔离的管道为当前管道相关联的破坏管道。
-                        id = libpointer('cstring',pipe_relative{i,2}{1,j});
-                        fprintf(fid,'隔离管道:%s\r\n',pipe_relative{i,2}{1,j} );
-                        index =libpointer('int32Ptr',0);
-                        [code,id,index]=calllib(lib_name,'ENgetlinkindex',id,index);
-                        if code
-                            disp(nem2str(code));
-                            keyboard
-                        end
-                        code=calllib(lib_name,'ENsetlinkvalue',index,11,0);%管道id状态为关闭
-                        if code
-                            disp(nem2str(code));
-                            fprintf(fid,'隔离管道:%s出错,代码%s\r\n',id,num2str(code) );
-                            keyboard
-                        end
-                    end
-                case 2
-                    %reopen
-                    str3 = [str3,'修复管道',pipe_relative{i,1},';'];
-                    id=libpointer('cstring',pipe_relative{i,1});
-                    index =libpointer('int32Ptr',0);
-                    [code,id,index]=calllib(lib_name,'ENgetlinkindex',id,index);
-                    
-                    if code
-                        disp(nem2str(code));
-                        keyboard
-                    end
-                    code= calllib(lib_name,'ENsetlinkvalue',index,11,1);
-                    fprintf(fid,'reopen管道%s,\r\n',pipe_relative{i,1});
-                    if code
-                        disp(nem2str(code));
-                        fprintf(fid,'reopen管道:%s出错,代码%s\r\n',id,num2str(code) );
-                        keyboard
-                    end
-            end
-        end
-        str = [str1,str2,str3];
-        fprintf(fid,'%s时刻,管道状态修改完毕\r\n',num2str(temp_t) );
-    else
-        str = '无动作';
-        fprintf(fid,'%s时刻,管道状态无需修改完毕\r\n',num2str(temp_t) );
-    end
-     [errcode4,temp_t] = calllib(lib_name,'ENrunH',temp_t);
-     [real_pre_chosen_node,cal_demand_chosen_node,req_demand_chosen_node]=Get_chosen_node_value_EPANETx64PDD(lib_name,node_id);
-     Pre{time_step_n} = real_pre_chosen_node;
-    Demand{time_step_n}=req_demand_chosen_node;
-    cal_Demand{time_step_n}=cal_demand_chosen_node;
-    system_serviceability_cell{time_step_n}= sum(cal_demand_chosen_node)/sum(req_demand_chosen_node);
-    activity_cell{time_step_n} = str;%记录每个时间步的行为
-    [code,temp_tstep]=calllib(lib_name,'ENnextH',temp_tstep);
+    errcode7_6 = calllib(lib_name,'ENsaveinpfile',output_net_pdd_file_n);-------------
+    errcode7_7 = calllib(lib_name,'ENsolveH');
+    calllib('epanet2','ENsaveH');%保存
+    calllib('epanet2','ENsetreport','NODES ALL'); % 设置输出报告的格式
+    calllib(lib_name,'ENsetstatusreport',2);
+    calllib('epanet2','ENreport'); %输出计算报告
+    [real_pre_chosen_node,cal_demand_chosen_node,req_demand_chosen_node]=Get_chosen_node_value_EPANETx64PDD(lib_name,node_id);
+    Pre{time_step_i} = real_pre_chosen_node;
+    Demand{time_step_i}=req_demand_chosen_node;
+    cal_Demand{time_step_i}=cal_demand_chosen_node;
+    system_serviceability_cell{time_step_i}= sum(cal_demand_chosen_node)/sum(req_demand_chosen_node);
 end
-
+% post-process
 system_serviceability_cell{1}
 system_serviceability_cell{end}
 % post-process
