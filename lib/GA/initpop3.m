5aa9fb3306b629ec149b4c5546d588b5faa70447
lookforhan <31067305+lookforhan@users.noreply.github.com>
2018-11-15 16:22:20 +0800

    Update lib/GA/initpop3.m

--- lib/GA/initpop3.m
+++ lib/GA/initpop3.m
@@ -8,10 +8,16 @@
 %初始化种群
 % pop=initpop2(20,{1;4;2;3;5})
 function [pop_isolation_init,pop_replacement_init]=initpop3(popsize,Pipe_isolation,Pipe_replacement)
-pop_isolation_init=cell(popsize,1);%第一列为管道检查次序
-pop_replacement_init=cell(popsize,1);%第一列为管道修复次序
-for i=1:popsize
-[pop_isolation_init{i,1}]=pipeDamage2priorityList2(Pipe_isolation);%随机生成检查次序
-[pop_replacement_init{i,1}]=pipeDamage2priorityList2(Pipe_replacement);%随机生成修复次序
+n_isolation = numel(Pipe_isolation);
+n_replacement = numel(Pipe_replacement);
+priority_isolation=zeros(n_isolation,popsize);%第一列为管道检查次序
+priority_replacement=zeros(n_replacement,popsize);%第一列为管道修复次序
+for i = 1:popsize
+    priority_isolation(:,i) = randperm(n_isolation);
+    priority_replacement(:,i) = randperm(n_replacement);
 end
+pop_isolation_init = Pipe_isolation(priority_isolation)';
+pop_replacement_init = Pipe_replacement(priority_replacement)';
+% 输出为矩阵，
+% 每一行为一个修复工作序列
 end
