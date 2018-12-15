792e0e70bb96f77b04995f788e4bf9504fa9fb0d
lookforhan <31067305+lookforhan@users.noreply.github.com>
2018-12-15 17:09:09 +0800

    Create greedy_pipe_isolation_priority.m

---
+++ lib/Class/greedy_pipe_isolation_priority.m
@@ -0,0 +1,12 @@
+classdef greedy_pipe_isolation_priority < handle
+    %greedy_pipe_priority 采用贪婪算法来确定破坏管道的修复次序    
+    %   1.确定隔离次序方法     
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
