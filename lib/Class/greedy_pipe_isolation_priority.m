be18af7c71eab110b1d7e5a6f3618116eabbc31c
lookforhan <31067305+lookforhan@users.noreply.github.com>
2018-12-16 23:29:01 +0800

    Update greedy_pipe_isolation_priority.m

--- lib/Class/greedy_pipe_isolation_priority.m
+++ lib/Class/greedy_pipe_isolation_priority.m
@@ -8,7 +8,6 @@ classdef greedy_pipe_isolation_priority < handle
     properties % 输入参数
         net_data % 管网信息
         break_pipe_id % 破坏管道信息
-        isolation_time % 需要隔离的管道的隔离时间
         inp_file % 破坏管网inp文件
         pipe_relative % 管道与新加管道关系
     end
@@ -22,13 +21,14 @@ classdef greedy_pipe_isolation_priority < handle
     properties % 中间参数
     end
     properties % 输出参数
+        output_inp_file % 每个输出的，隔离一个管道及相关管道的管网
+        output_system_serviceablity % 输出的隔离一个管道对应的供水满足率
     end
     methods
-        function obj = greedy_pipe_isolation_priority(inp_file,net_data,break_pipe_id,isolation_time_mat,pipe_relative)
+        function obj = greedy_pipe_isolation_priority(inp_file,net_data,break_pipe_id,pipe_relative)
             obj.inp_file = inp_file;
             obj.net_data = net_data;
-            obj.break_pipe_id = break_pipe_id;
-            obj.isolation_time = isolation_time_mat;
+            obj.break_pipe_id = break_pipe_id;  
             obj.pipe_relative = pipe_relative;
             if libisloaded(obj.lib_name)
                 disp([obj.lib_name,'is loaded.'])
@@ -37,10 +37,46 @@ classdef greedy_pipe_isolation_priority < handle
             end
         end
         function Run(obj)
+            fid = 1;         
             for i = 1: obj.n_break_pipe
+                obj.errcode.Run.ENopen = calllib(obj.lib_name,'ENopen',obj.inp_file,[obj.inp_file(1:end-4),'_Run','.rpt'],[obj.inp_file(1:end-4),'_Run','.out']);
+                obj.errcode.Run.ENsettimeparam = calllib(obj.lib_name,'ENsettimeparam',0,0);% 设置模拟历时
+                obj.errcode.Run.ENsetoption = calllib(obj.lib_name,'ENsetoption',0,500);% 动态链接库中迭代次数
+                the_current_break_id = obj.break_pipe_id{i,1};
+                obj.output_inp_file{i} = [obj.inp_file(1:end-4),'_isolate_',the_current_break_id,'.inp'];
+                [~,the_current_break_id_loc] = ismember(the_current_break_id,obj.pipe_relative(:,1));
+                for j = 1:numel(obj.pipe_relative{the_current_break_id_loc,2})
+                    id = libpointer('cstring',obj.pipe_relative{the_current_break_id_loc,2}{1,j});
+                    fprintf(fid,'隔离管道:%s\r\n',obj.pipe_relative{the_current_break_id_loc,2}{1,j} );
+                    index =libpointer('int32Ptr',0);
+                    [code,id,index]=calllib(obj.lib_name,'ENgetlinkindex',id,index);
+                    if code
+                        disp(num2str(code));
+                        keyboard
+                    end
+                    code=calllib(obj.lib_name,'ENsetlinkvalue',index,11,0);%管道id状态为关闭
+                    if code
+                        disp(num2str(code));
+                        fprintf(fid,'隔离管道:%s出错,代码%s\r\n',id,num2str(code) );
+                        keyboard
+                    end
+                    code= calllib(obj.lib_name,'ENsetlinkvalue',index,4,0);
+                    if code
+                        disp(num2str(code));
+                        fprintf(fid,'隔离管道:%s出错,代码%s\r\n',id,num2str(code) );
+                        keyboard
+                    end
+                end
+                obj.errcode.Run.ENsolveH = calllib(obj.lib_name,'ENsolveH');
+                obj.errcode.Run.ENsaveinpfile = calllib(obj.lib_name,'ENsaveinpfile',obj.output_inp_file{i});
+                [~,cal_demand_chosen_node,req_demand_chosen_node]=Get_chosen_node_value_EPANETx64PDD(obj.lib_name,obj.node_id);
+                obj.output_system_serviceablity(i,1) = sum(cal_demand_chosen_node)/sum(req_demand_chosen_node);
+                obj.errcode.Run.ENclose = calllib(obj.lib_name,'ENclose');
             end
+            
         end
     end
+    
     methods % get
         function n_break_pipe = get.n_break_pipe(obj)
             n_break_pipe = numel(obj.break_pipe_id);
@@ -52,20 +88,18 @@ classdef greedy_pipe_isolation_priority < handle
             input_check.break_pipe_id_with_pipe_relative = all(ismember(obj.break_pipe_id,obj.pipe_relative(:,1)));
             input_check.pipe_relative_with_pipe_id = all(ismember(obj.pipe_relative(:,1),obj.pipe_id));
         end
-
         function pipe_id = get.pipe_id(obj)
             pipe_id = obj.net_data{5,2}(:,1);
         end
-
         function current_system_serviceablity = get.current_system_serviceablity(obj)
-            obj.errcode.ENopen = calllib(obj.lib_name,'ENopen',obj.inp_file,[obj.inp_file(1:end-4),'current','.rpt'],'');
-            obj.errcode.ENsettimeparam = calllib(obj.lib_name,'ENsettimeparam',0,0);% 设置模拟历时
-            obj.errcode.ENsetoption = calllib(obj.lib_name,'ENsetoption',0,500);% 动态链接库中迭代次数
-            obj.errcode.ENsloveH = calllib(obj.lib_name,'ENsolveH');
+            obj.errcode.CSS.ENopen = calllib(obj.lib_name,'ENopen',obj.inp_file,[obj.inp_file(1:end-4),'current','.rpt'],'');
+            obj.errcode.CSS.ENsettimeparam = calllib(obj.lib_name,'ENsettimeparam',0,0);% 设置模拟历时
+            obj.errcode.CSS.ENsetoption = calllib(obj.lib_name,'ENsetoption',0,500);% 动态链接库中迭代次数
+            obj.errcode.CSS.ENsloveH = calllib(obj.lib_name,'ENsolveH');
             [~,cal_demand_chosen_node,req_demand_chosen_node]=Get_chosen_node_value_EPANETx64PDD(obj.lib_name,obj.node_id);
             current_system_serviceablity = sum(cal_demand_chosen_node)/sum(req_demand_chosen_node);
-            obj.errcode.ENreport = calllib(obj.lib_name,'ENreport');
-            obj.errcode.ENclose = calllib(obj.lib_name,'ENclose');
+            obj.errcode.CSS.ENreport = calllib(obj.lib_name,'ENreport');
+            obj.errcode.CSS.ENclose = calllib(obj.lib_name,'ENclose');
         end
     end
 end
