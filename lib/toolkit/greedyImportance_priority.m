b9ccaf1123ef68e965a949be1c90ed5393a054d1
lookforhan <31067305+lookforhan@users.noreply.github.com>
2019-01-28 21:24:34 +0800

    Update greedyImportance_priority.m

--- lib/toolkit/greedyImportance_priority.m
+++ lib/toolkit/greedyImportance_priority.m
@@ -5,10 +5,12 @@ break_pipe_id = damage_pipe_info{5}(damage_pipe_info{3}==2);
 isolation_time_mat = Dp_Inspect_mat(damage_pipe_info{3}==2);
 best_indivi_isolation_1_cell = cell(numel(break_pipe_id),1);
 the_chose_isolation_inp = cell(numel(break_pipe_id),1);
-
+tic
 for i = 1:numel(break_pipe_id)  
+    timecost = toc;
+    disp(['=======隔离第',num2str(i),'/',num2str(numel(break_pipe_id) ),'步；时间',num2str(timecost),'S=========='])
     mid_class = greedy_pipe_isolation_priority(temp_inp_file,net_data,break_pipe_id,pipe_relative);
-    mid_class.input_check
+    mid_class.input_check;
     mid_class.out_dir = '.\results\';
     mid_class.Run
     system_s = mid_class.output_system_serviceablity;
@@ -28,8 +30,10 @@ best_indivi_recovery_1_cell = cell(numel(recovery_pipe_id),1);
 the_chose_recovery_inp = cell(numel(recovery_pipe_id),1);
 
 for j = 1:numel(recovery_pipe_id)
+    timecost = toc;
+    disp(['=======修复第',num2str(j),'/',num2str(numel(recovery_pipe_id)),'步；时间',num2str(timecost),'S=========='])
     mid_recovery_class = greedy_pipe_recovery_priority(temp_inp_file,net_data,recovery_pipe_id,pipe_relative);
-    mid_recovery_class.input_check
+    mid_recovery_class.input_check;
     mid_recovery_class.out_dir = '.\results\';
     mid_recovery_class.Run
     system_s = mid_recovery_class.output_system_serviceablity;
