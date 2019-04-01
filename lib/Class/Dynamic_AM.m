ac6c3d3be8f048d6f902f1ca818f9b605079834a
lookforhan <31067305+lookforhan@users.noreply.github.com>
2019-04-01 22:08:19 +0800

    Create Dynamic_AM.m

---
+++ lib/Class/Dynamic_AM.m
@@ -0,0 +1,39 @@
+classdef Dynamic_AM < handle
+    %Dynamic_AM  Dynamic_Adjacency Matrix
+    %   动态记录修复过程中管网邻接矩阵
+    %   检查修复过程中的孤立节点    
+    properties
+        adj_mat % 邻接矩阵
+        
+    end
+    properties % input
+        
+        net_data % 初始管网数据
+    end
+    properties
+        init_adj_mat % 初始管网邻接矩阵
+    end
+    properties(Dependent,SetAccess = private) % 中间变量
+        
+        end_time % 时间表中的最大时间
+        pipe_node % 管道信息，节点与节点的连接关系
+        reservoir % 水库的编号
+        all_node % 所有节点的编号
+    end
+    properties(SetAccess = private) % 输出
+        isol_node % isolated node 孤立节点
+    end
+    methods
+        function obj = Dynamic_AM(Net_data)
+            obj.net_data = Net_data;
+        end
+        function Run(obj)
+        end
+    end
+    methods
+        function pipe_node = get.pipe_node(obj)
+        end
+    end
+    
+end
+
