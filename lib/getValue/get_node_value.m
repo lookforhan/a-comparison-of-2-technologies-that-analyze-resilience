87ce66cda9f8f3dcbfac3d5b43d83bcfb2f95480
lookforhan <31067305+lookforhan@users.noreply.github.com>
2019-05-05 21:16:27 +0800

    Update get_node_value.m

--- lib/getValue/get_node_value.m
+++ lib/getValue/get_node_value.m
@@ -2,9 +2,15 @@ function [ node_original_data ] = get_node_value( inpfile,libName )
 %get_node_value 此处显示有关此函数的摘要
 %   此函数需要先加载动态链接库epanet2.dll
 %   输出节点的id / 需水量/压力
-funcName = 'get_node_value';
+% funcName = mfilename;
 t=calllib(libName,'ENopen',inpfile,'high.rpt','high.out');
+if t~=0
+    keyboard
+end
 code=calllib(libName,'ENsolveH');% 运行水力计算
+if code~=0
+    keyboard
+end
 calllib(libName,'ENsaveH');% 保存
 count=libpointer('int32Ptr',0);%指针参数--计数，中间变量
 [~,count_node]=calllib(libName,'ENgetcount',0,count);%全部节点个数
@@ -13,10 +19,11 @@ original_junction_num=count_node-count_tank;%用户节点个数
 node_original_data=cell(original_junction_num,3);%节点id和初始需水量
 node_original_dem=zeros(original_junction_num,1);
 node_original_pre=zeros(original_junction_num,1);
-node_id_k=libpointer('cstring','node_id_k');
-value_dem=libpointer('singlePtr',0);
-value_pre=libpointer('singlePtr',0);
+
 for k=1:original_junction_num
+    node_id_k=libpointer('cstring','node_id_k');
+value_dem=libpointer('doublePtr',0);
+value_pre=libpointer('doublePtr',0);
     [~,node_id_k]=calllib(libName,'ENgetnodeid',k,node_id_k);
     node_original_data{k,1}=node_id_k;
     [~,value_dem]=calllib(libName,'ENgetnodevalue',k,1,value_dem);
